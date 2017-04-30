4a2aaf69547f6e4633f012afbe76a1afe9c87491
Leandro Oliveira <oliveira.dev1997@gmail.com>
2017-04-29 22:26:58 -0300

    Inserção das listagens nos menus

--- config/permissoes.php
+++ config/permissoes.php
@@ -1,6 +1,7 @@
 <?php
 
-$permissoes = ['config.php'				=> [3],
+$permissoes = ['index.php'				=> [1, 2, 3],
+			   'config.php'				=> [3],
 			   'conf_permissao.php' 	=> [3],
  			   'conf_instituicao.php'	=> [3],	
  			   'add_profissional.php'	=> [1, 3],
@@ -8,4 +9,7 @@ $permissoes = ['config.php'				=> [3],
  			   'add_paciente.php'		=> [1, 2, 3],
  			   'add_testes.php'			=> [1, 2, 3],
  			   'rel_categorias.php'		=> [1, 2, 3],
+ 			   'pacientes.php'			=> [1, 2, 3],
+ 			   'profissionais.php'	    => [1, 3],
+ 			   'responsaveis.php'		=> [1, 2, 3],
 			  ];
--- tmp/views/menu.php
+++ tmp/views/menu.php
@@ -1,9 +1,12 @@
 <?php 
 
 	$menu = [
-				["../tmp/add_profissional.php"	, 'Adicionar Profissional', 'add'],
-				["../tmp/add_responsavel.php" 	, 'Adicionar Responsável' , 'add1'],
-				["../tmp/add_paciente.php" 		, 'Adicionar Paciente'	  , 'add2'],
+				//["../tmp/add_profissional.php"	, 'Adicionar Profissional', 'add'],
+				//["../tmp/add_responsavel.php" 	, 'Adicionar Responsável' , 'add1'],
+				//["../tmp/add_paciente.php" 		, 'Adicionar Paciente'	  , 'add2'],
+				["../tmp/profissionais.php"		, 'Profissionais'		  , 'add'],
+				["../tmp/responsaveis.php" 		, 'Responsáveis' 		  , 'add1'],
+				["../tmp/pacientes.php" 		, 'Pacientes'			  , 'add2'],
 				["../tmp/add_testes.php" 		, 'Adicionar Testes'      , 'add3'],
 				["../tmp/rel_categorias.php"    , 'Relatorios'            , 'add4'],
 				["../tmp/config.php"  			, 'Configuração'          , 'add5'],
